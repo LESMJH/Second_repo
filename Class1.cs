6f65d607efdbe66a18634fb10e30a0f9fd1c4068
Martin Hilligenn <9186@ctucareer.co.za>
2022-08-04 16:10:36 +0200

    Created second repo

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractice.Second_repo
+{
+    class Class1
+    {
+        public string second = "this is my second repository";
+    }
+}
